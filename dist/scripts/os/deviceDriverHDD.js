e479a12539c8ea3989ce66234b71507a9ed50cd2
dblossom <daniel.blossom@gmail.com>
2014-11-21 15:26:32 -0500

    working on format, not tested but think it will work ... made an update display, to well update the hdd display. making progress...

--- dist/scripts/os/deviceDriverHDD.js
+++ dist/scripts/os/deviceDriverHDD.js
@@ -29,19 +29,101 @@ var TSOS;
         DeviceDriverHDD.prototype.setDriverEntry = function () {
             if (this.supports_html5_storage()) {
                 // init the vars: could not do it above ... why?
+                // how many tracks
                 this.TRACKS = 4;
+
+                // how many sectors
                 this.SECTORS = 8;
+
+                // how many blocks
                 this.BLOCKS = 8;
+
+                // how many bytes per block
                 this.BYTES_BLOCKS = 64;
+
+                // meta data -- (in use, t, s, b) === 4
+                this.meta = 4;
+
+                // where we will store our stuff.
                 this.HardDriveArray = window.localStorage;
 
+                // sure why not.
                 this.status = "loaded";
+
+                // hhd starts off unformatted
+                this.isFormatted = false;
             } else {
                 //TODO: error
             }
         };
 
+        /**
+        * sets the ISR - currently serves no purpose
+        */
         DeviceDriverHDD.prototype.setISR = function () {
+        };
+
+        /**
+        * A method to write data to the disk
+        * @param track, sector, block - corresponds to location to write
+        *        data is what to write
+        */
+        DeviceDriverHDD.prototype.write = function (track, sector, block, data) {
+            var key = String(track) + String(sector) + String(block);
+            this.HardDriveArray.setItem(key, data);
+            this.updateHDDDisplay(_HDDdisplay, track, sector, block, this.padZeros(data));
+        };
+
+        /**
+        * A method to read and return what is at a location on disk'
+        * @param track, sector, block - where to read
+        * @return what is there, whether you agree or not.
+        */
+        DeviceDriverHDD.prototype.read = function (track, sector, block) {
+            var key = String(track) + String(sector) + String(block);
+            return this.HardDriveArray.getItem(key);
+        };
+
+        /**
+        * A method to format the hard-disk-drive
+        */
+        DeviceDriverHDD.prototype.format = function () {
+            if (!this.supports_html5_storage()) {
+                return (this.isFormatted = false);
+            } else {
+                for (var t = 0; t < this.TRACKS; t++) {
+                    for (var s = 0; s < this.SECTORS; s++) {
+                        for (var b = 0; b < this.BLOCKS; b++) {
+                            this.write(t, s, b, this.zeros());
+                        }
+                    }
+                }
+                return (this.isFormatted = true);
+            }
+        };
+
+        /**
+        * Just so I do not need to type 64 zeros all the time - I just call this
+        * TODO: tweak is so it is bytes - meta...so 60 maybe? not sure until we
+        *       start to need the meta data
+        */
+        DeviceDriverHDD.prototype.zeros = function () {
+            var zero = "0";
+            for (var i = 1; i < (this.BYTES_BLOCKS - this.meta); i++) {
+                zero = zero + "0";
+            }
+            return zero;
+        };
+
+        /**
+        * A method to pad the number of zeros at the end of the string
+        */
+        DeviceDriverHDD.prototype.padZeros = function (toPad) {
+            while (toPad.length < (this.BYTES_BLOCKS - this.meta)) {
+                toPad = toPad + "0";
+            }
+
+            return toPad;
         };
 
         /**
@@ -53,6 +135,23 @@ var TSOS;
                 return 'localStorage' in window && window['localStorage'] !== null;
             } catch (e) {
                 return false;
+            }
+        };
+
+        DeviceDriverHDD.prototype.updateHDDDisplay = function (tblElement, t, s, b, data) {
+            var TSB = String(t) + String(s) + String(b);
+
+            for (var i = 0; i < (tblElement.rows.length - 1); i++) {
+                var row = null;
+                row = tblElement.rows[i];
+                var rowcells = null;
+                rowcells = row.cells;
+
+                if (TSB === rowcells[0].innerHTML) {
+                    rowcells[2].innerHTML = data;
+
+                    break;
+                }
             }
         };
 
@@ -85,9 +184,12 @@ var TSOS;
 
                 row.insertCell(0).innerHTML = temp;
 
-                row.insertCell(1).innerHTML = "0.0.0";
+                row.insertCell(1).innerHTML = "000";
                 row.insertCell(2).innerHTML = "0000000000000000000000000000000000000000000000000000000000000000";
             }
+
+            this.write(0, 0, 1, "hi");
+            //  this.write(0,0,5,"bye");
         };
         return DeviceDriverHDD;
     })(TSOS.DeviceDriver);
--- source/scripts/os/deviceDriverHDD.ts
+++ source/scripts/os/deviceDriverHDD.ts
@@ -22,8 +22,12 @@ module TSOS {
         public BLOCKS:number;
         // bytes
         public BYTES_BLOCKS:number;
+        // meta-data
+        public meta:number;
         // array for hard-drive
         public HardDriveArray;
+        // is the drive formated
+        public isFormatted:boolean;
         
         /**
          * The constructor for our HDD driver
@@ -42,21 +46,98 @@ module TSOS {
         
             if(this.supports_html5_storage()){
                 // init the vars: could not do it above ... why?
+                
+                // how many tracks
                 this.TRACKS = 4;
+                // how many sectors
                 this.SECTORS = 8;
+                // how many blocks
                 this.BLOCKS = 8;
+                // how many bytes per block
                 this.BYTES_BLOCKS = 64;
+                // meta data -- (in use, t, s, b) === 4
+                this.meta = 4;
+                // where we will store our stuff.
                 this.HardDriveArray = window.localStorage;
-        
+                // sure why not.
                 this.status = "loaded";
+                // hhd starts off unformatted
+                this.isFormatted = false;
             }else{
                 //TODO: error
             }
             
         }
         
-        public setISR():void{
+        /**
+         * sets the ISR - currently serves no purpose
+         */
+        public setISR():void{ }
+        
+        /**
+         * A method to write data to the disk
+         * @param track, sector, block - corresponds to location to write
+         *        data is what to write
+         */
+        public write(track:number, sector:number, block:number, data:string){
+            var key:string = String(track) + String(sector) + String(block);
+            this.HardDriveArray.setItem(key, data);
+            this.updateHDDDisplay(_HDDdisplay, track, sector, block, this.padZeros(data));
+        }
+        
+        /**
+         * A method to read and return what is at a location on disk'
+         * @param track, sector, block - where to read
+         * @return what is there, whether you agree or not.
+         */
+        public read(track:number, sector:number, block:number):string{
+            var key:string = String(track) + String(sector) + String(block);
+            return this.HardDriveArray.getItem(key);
+        }
+        
+        /**
+         * A method to format the hard-disk-drive
+         */
+        public format():boolean{
+                        
+            if(!this.supports_html5_storage()){
+                return (this.isFormatted = false);
+            }else{
+                for(var t:number = 0; t < this.TRACKS; t++){
+                    for(var s:number = 0; s < this.SECTORS; s++){
+                        for(var b:number = 0; b < this.BLOCKS; b++){
+                            this.write(t, s, b, this.zeros());
+                        }
+                    }
+                }
+                return (this.isFormatted = true);
+            }
+        }
+        
+        /**
+         * Just so I do not need to type 64 zeros all the time - I just call this
+         * TODO: tweak is so it is bytes - meta...so 60 maybe? not sure until we
+         *       start to need the meta data
+         */
+        private zeros():string {
+            var zero:string = "0";
+            for(var i:number = 1; i < (this.BYTES_BLOCKS - this.meta); i++){
+                zero = zero + "0";
+            }
+            return zero;
+        }
+        
+        /**
+         * A method to pad the number of zeros at the end of the string
+         */
+        private padZeros(toPad:string):string{
+        
+            while(toPad.length < (this.BYTES_BLOCKS - this.meta)){
+                toPad = toPad + "0";
+            }
             
+            return toPad;
+        
         }
         
         /**
@@ -68,6 +149,27 @@ module TSOS {
                 return 'localStorage' in window && window['localStorage'] !== null;
             } catch (e) {
                 return false;
+            }
+        }
+        
+        public updateHDDDisplay(tblElement: HTMLTableElement, t:number, s:number, b:number, data:string):void{
+            
+            
+            var TSB: string = String(t) + String(s) + String(b);
+            
+            for(var i:number = 0; i < (tblElement.rows.length - 1); i++){
+                
+                var row = null;
+                row = tblElement.rows[i];
+                var rowcells = null;
+                rowcells = row.cells;
+                
+                if(TSB === rowcells[0].innerHTML){
+                    
+                    rowcells[2].innerHTML = data;
+                    
+                    break;
+                }
             }
         }
         
@@ -101,9 +203,14 @@ module TSOS {
                 
                 row.insertCell(0).innerHTML = temp;
                 
-                row.insertCell(1).innerHTML = "0.0.0";
+                row.insertCell(1).innerHTML = "000";
                 row.insertCell(2).innerHTML = "0000000000000000000000000000000000000000000000000000000000000000";
             }
+            
+            this.write(0,0,1,"hi");
+          //  this.write(0,0,5,"bye");
+            
+            
         }
         
     }
